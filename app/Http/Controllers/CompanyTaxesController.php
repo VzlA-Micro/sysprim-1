d5ed668bd77a1bc591d894df4ed6ee9df07b6937
YohnnD <grabieldiaz63@gmail.com>
2019-10-21 16:04:27 -0400

    Login en taxes

--- app/Http/Controllers/CompanyTaxesController.php
+++ app/Http/Controllers/CompanyTaxesController.php
@@ -23,6 +23,12 @@ class CompanyTaxesController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+
+
+    public function __construct(){
+        $this->middleware('auth');
+    }
+
     public function index(){
         $taxes=Taxe::all();
     }
