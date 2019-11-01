39b1f67e1aace4fea4c11d193d12c4de519507d9
YohnnD <grabieldiaz63@gmail.com>
2019-11-01 14:45:30 -0400

    se agrego que solo se puede pagar mes actual, en el sistema.

--- app/Helpers/TaxesMonth.php
+++ app/Helpers/TaxesMonth.php
@@ -15,7 +15,6 @@ class TaxesMonth{
         $date = null;
         $company = Company::find($id);
 
-
         $dayMoraEspecial=20;//el dia de cobro para lo que tienen mora y son agente de retencion
         $dayMoraNormal=14;//el dia de cobro para lo que no son agente de retención
         $diffDayMora=0;
@@ -24,16 +23,10 @@ class TaxesMonth{
 
         if ($companyTaxes->isEmpty()) {//si no tiene pagos
 
-            $fiscal_period = Carbon::parse($company->created_at);//utilizo la fecha que se creo el registro como referencia si esta atrasado o no
+            $fiscal_period = Carbon::parse('2019-12-01');//utilizo la fecha que se creo el registro como referencia si esta atrasado o no
 
-
-            $now = Carbon::now();//fecha del computador
-
-            $diffMount = $fiscal_period->diffInMonths($now);//veo la diferencia de meses
-            if($diffMount>=1){
-
-                $mes=self::$mounths[($fiscal_period->format('m'))-1];
-
+                $now = Carbon::now();//fecha del computador
+                $mes=self::$mounths[($now->format('m')-1)-1];
 
                 if($company->typeCompany==='R'){
                     if($now->format('d')<=$dayMoraEspecial){
@@ -50,24 +43,17 @@ class TaxesMonth{
                         $mora=true;
                     }
                 }
-
-
+                $now->subMonth(1);
                 $date =array(
-                    'mount_pay' =>$mes.'-'.$fiscal_period->format('Y'), 'mount_diff' =>
-                    $diffMount,'fiscal_period'=>$fiscal_period->format('Y-m-d'), 'mora'=>$mora,
+                    'mount_pay' =>$mes.'-'.$now->format('Y'),
+                    'fiscal_period'=>$now->format('Y-m-d'),
+                    'mora'=>$mora,
                     'diffDayMora'=>$diffDayMora
-                    );
-            }else{
-                $date=null;
-                $mes=null;
-            }
-
-
+                );
         } else {//si tiene datos
-
             $fiscal_period = Carbon::parse($companyTaxes[0]->fiscal_period);//utilizo el ultimo pago realido valido y lo tomo como refencia
             $now = Carbon::now();//fecha del computador
-            $diffMount = $fiscal_period->diffInMonths($now);
+
 
             if($company->typeCompany==='R'){
                 if($now->format('d')<=$dayMoraEspecial){
@@ -86,26 +72,26 @@ class TaxesMonth{
             }
 
 
-            if ($diffMount >= 1 || $companyTaxes[0]->status!==null) {
-                if ($companyTaxes[0]->status === 'verified' && $diffMount > 1) {
-                    $diffMount--;//resto 1 a la diferencia de mes porque este utilimo esta pago
-                    $fiscal_period->addMonth(1);//añado un mes para saber cual es el proximo a pagar
-                    $mes=self::$mounths[($fiscal_period->format('m'))-1];
-                    $date = array('mount_pay' => $mes.'-'.$fiscal_period->format('Y'), 'mount_diff' => $diffMount,'fiscal_period'=>$fiscal_period->format('Y-m-d'),  'mora'=>$mora, 'diffDayMora'=>$diffDayMora);
-                } else if ($companyTaxes[0]->status === 'verified') {//si no esta vacio y el pago esta verificado,y no hay diferencia de mes, esta al dia.
+            if ($companyTaxes[0]->status!==null) {
+                $now->subMonth(1);
+                if ($companyTaxes[0]->status === 'verified' && $companyTaxes[0]->fiscal_period==$now->format('Y-m-d')) {
                     $date = null;
                     $mes=null;
-                } else {
-                    $mes=self::$mounths[($fiscal_period->format('m'))-1];
-                    $date = array('mount_pay' => $mes.'-'.$fiscal_period->format('Y'),
-                        'mount_diff' => $diffMount,'fiscal_period'=>$fiscal_period->format('Y-m-d'), 'mora'=>$mora, 'diffDayMora'=>$diffDayMora);
+                } else{
+                    $mes=self::$mounths[($now->format('m'))-1];
+                    $date = array(
+                        'mount_pay' => $mes.'-'.$now->format('Y'),
+                        'fiscal_period'=>$now->format('Y-m-d'),
+                        'mora'=>$mora,
+                        'diffDayMora'=>$diffDayMora);
+
                 }
             }
 
         }
 
         if($notification){
-            self::createNotification($company->name, $mes, $diffMount, $date);
+            self::createNotification($company->name, $mes, $date);
         }else{
             return $date;
         }
@@ -114,21 +100,10 @@ class TaxesMonth{
     }
 
 
-    public  static  function createNotification($name,$mes,$diffMount,$data){
+    public  static  function createNotification($name,$mes,$data){
         $notifications=Notification::where('type_notification','date-'.$mes)->where('title',$name)->get();
         if($data!=null&&($notifications->isEmpty()||$notifications[0]->title!=$name)){
-            $notification=new Notification();
-            $notification->type_notification='date-'.$mes;
-            $notification->title=$name;
-            if($diffMount==1){
-                $notification->content="Estimado contribuyente, esta atrasado en sus pago por ". $diffMount ." mes,<br>por favor cancele el mes de ".$mes.".";
-            }else{
-                $notification->content="Estimado contribuyente, esta atrasado en sus pago por ". $diffMount ." meses,<br>por favor cancele el mes de ".$mes.".";
-            }
 
-            $notification->view=0;
-            $notification->user_id=\Auth::user()->id;
-            $notification->save();
         }
 
     }
