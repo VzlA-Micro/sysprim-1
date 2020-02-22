d3a3ff3ac3fcdb196c68477b6b01f45cf5eb15fc
AMSG2647 <angelmserranog@gmail.com>
2020-02-22 12:52:00 -0400

    Cambios en linea de tiempo terreno

--- resources/views/modules/catastral-construction/manage.blade.php
+++ resources/views/modules/catastral-construction/manage.blade.php
@@ -12,14 +12,14 @@
                     <li class="breadcrumb-item"><a href="{{ route('home') }}">Inicio</a></li>
                     <li class="breadcrumb-item"><a href="{{ route('settings.manage') }}">Configuración</a></li>
                     <li class="breadcrumb-item"><a href="{{ route('settings.property') }}">Configuración de Inmuebles Urbanos</a></li>
-                    <li class="breadcrumb-item"><a href="{{ route('catrastal.construction.manage') }}">Gestionar Valor  Catastral de Contrucción</a></li>
+                    <li class="breadcrumb-item"><a href="{{ route('catrastal.construction.manage') }}">Gestionar Valor  Catastral de Construcción</a></li>
                 </ul>
             </div>
             @can('Registrar Valor Construccion')
             <div class="col s12 m4 animated bounceIn">
                 <a href="{{ route('catrastal.construction.register') }}" class="btn-app white cyan-text">
                     <i class="icon-create_new_folder"></i>
-                    <span class="truncate">Registrar Valor  Catastral de Contrucción</span>
+                    <span class="truncate">Registrar Valor  Catastral de Construcción</span>
                 </a>
             </div>
             @endcan
@@ -27,7 +27,7 @@
             <div class="col s12 m4 animated bounceIn">
                 <a href="{{ route('catrastal.construction.read') }}" class="btn-app white amber-text">
                     <i class="icon-format_list_bulleted"></i>
-                    <span class="truncate">Ver Valor  Catastral de Contrucción</span>
+                    <span class="truncate">Ver Valor  Catastral de Construcción</span>
                 </a>
             </div>
             @endcan
