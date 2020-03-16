c9f9fe8b2e0913f809ec098ebf3df4cfc3d645fa
YohnnD <grabieldiaz63@gmail.com>
2020-03-16 08:35:52 -0400

    generar planillas

--- public/js/owner.js
+++ public/js/owner.js
@@ -6,7 +6,7 @@ $(window).on("load", function () {
 $(document).ready(function() {
     if( localStorage.getItem('url')===null){
         // localStorage.setItem('url','https://172.19.50.253/');
-        localStorage.setItem('url','http://sysprim.com.devel/');
+        localStorage.setItem('url','https://sysprim.com/');
 
     }
 
