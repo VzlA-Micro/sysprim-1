5d70c329a51ae13dbe6c6e15ade544425199273b
YohnnD <tecnova080919@barquimetro.us-central1-a.c.magnetic-mender-252400.internal>
2020-03-16 18:47:24 +0000

    Cambioes del servidor

---
+++ public/js/data/tax-unit.js
@@ -0,0 +1,89 @@
+$(document).ready(function () {
+    var url = localStorage.getItem('url');
+
+    $('select').formSelect();
+    var date = new Date();
+
+    $('#since_date').datepicker({
+        maxDate: date,
+        format: 'yyyy-mm-dd', // Configure the date format
+        // yearRange: [1900,date.getFullYear()],
+        showClearBtn: false,
+        i18n: {
+            cancel: 'Cerrar',
+            clear: 'Reiniciar',
+            done: 'Hecho',
+            months: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+            monthsShort: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'],
+            weekdays: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+            weekdaysShort: ['Dom', 'Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sab'],
+            weekdaysAbbrev: ['D', 'L', 'M', 'M', 'J', 'V', 'S']
+        }
+    });
+
+
+    $('#to_date').datepicker({
+        maxDate: null,
+        format: 'yyyy-mm-dd', // Configure the date format
+        minDate: date,
+        showClearBtn: false,
+        i18n: {
+            cancel: 'Cerrar',
+            clear: 'Reiniciar',
+            done: 'Hecho',
+            months: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+            monthsShort: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'],
+            weekdays: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+            weekdaysShort: ['Dom', 'Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sab'],
+            weekdaysAbbrev: ['D', 'L', 'M', 'M', 'J', 'V', 'S']
+        }
+    });
+
+    // Registrar
+	$('#register').submit(function(e) {
+		e.preventDefault();
+		var formData = new FormData(this);
+		$.ajax({
+			url: url + "tax-unit/save",
+            cache: false,
+            contentType: false,
+            processData: false,
+            data: formData,
+            method: "POST",
+            beforeSend: function() {
+            	$("#preloader").fadeIn('fast');
+                $("#preloader-overlay").fadeIn('fast');
+            },
+            success: function(resp) {
+                console.log(resp);
+                if(resp.status == 'success') {
+                    swal({
+                        title: "¡Bien Hecho!",
+                        text: resp.message,
+                        icon: "success",
+                        button: {
+                            text: "Esta bien",
+                            className: "green-gradient"
+                        }
+                    }).then(function (accept) {
+                        window.location.href = url + "tax-unit/manage";
+                    });
+                }
+             },
+            error: function(err) {
+            	console.log(err);
+                $("#preloader").fadeOut('fast');
+                $("#preloader-overlay").fadeOut('fast');
+                swal({
+                    title: "¡Oh no!",
+                    text: "Ocurrio un error inesperado, refresque la pagina e intentenlo de nuevo.",
+                    icon: "error",
+                    button:{
+                        text: "Entendido", 
+                        className: "red-gradient"
+                    },
+                });
+            }
+		});
+	});
+});
